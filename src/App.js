cdc6d0144e92010eb0f3e36df837e78d9664791d
Danielscg2012@gmail.com <Danielscg2012@gmail.com>
2022-11-02 08:47:59 -0300

    consumi a api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import Loading from "./assets/loading.svg"
+
 
 function App() {
+  
+  const [dog, setDog] = useState(null);
+  const [open, setOpen] = useState(false);
+  const API = "https://dog.ceo/api/breeds/image/random"
+  useEffect(() => {
+  
+    axios
+     .get(API)
+ 
+    .then((response) => {
+      setDog(response.data.message);
+      console.log(response)
+    })
+    .catch(e => console.log("deu ruim! 😢", e));
+  }, [] );
+  
+ 
+
+  function buildImage() {
+ 
+     if (dog === null) return <img src={Loading} alt="carregando..." />
+     if (dog.length === 0) return <h1>Poxa, que peninha! Não há produtos cadastrados</h1>
+  
+        return( 
+          
+        <img src={dog} alt="imagem de cachorro" />
+      
+        )
+  
+  }
+
+
+  const image = buildImage();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="view">
+      <div className="image">
+        {image}
+      </div>
+   
+     
     </div>
   );
 }
